f3c94b9db4a37c629ad5b93d8f7d15f5108dddae
Thomas L <148439815+thomasleavy@users.noreply.github.com>
2024-09-01 20:34:24 +0000

    tidying up about 01-hello.go

--- 01-hello/01-hello.go
+++ 01-hello/01-hello.go
@@ -8,7 +8,7 @@ To verify if this has been downloaded, open the CMD prompt or PowerShell and typ
 If necessary, you might need to set up Go Environmental Variables. Search for "Environmental Variables" from the Start menu of you device. In the "System Variables" section, find the "Path" variable and edit it. Click "New" and add "C:\Go\bin". Press OK and close. Open CMD prompt or PowerShell and type "go version" to verify the path has been set up properly.
 */
 /*
-If you are using Visual Studio Code (VSCode), click the Extensions icon at the left sidebar and search for the Go Team at Google extension and install it.
+If you are using Visual Studio Code (VSCode), click the Extensions icon at the left sidebar and search for the "Go Team at Google" extension and install it.
 */
 /*
 Create a new folder, for example, GoProjects. Create a new file called "01-hello.go". Then you can use the below code.
